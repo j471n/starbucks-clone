00cb80de8e0ec4f11f3e4d7fb9bc05aaad5de98c
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2020-11-15 11:59:59 +0530

    Making Hamburgur Work in Mobile Responsive

--- links.js
+++ links.js
@@ -1,7 +1,31 @@
 const findStore = document.getElementById('find-store');
+const hamButton = document.getElementById('ham');
+const bar1 = document.getElementById('bar1');
+const bar2 = document.getElementById('bar2');
+const bar3 = document.getElementById('bar3');
+const MobileNav = document.getElementById("myNav");
+const close = document.getElementById('close');
 
 //? Redirecting to Starbucks website
 findStore.addEventListener('click', () => {
     url = 'http://www.starbucks.in/store-locator';
     window.open(url);
 });
+
+hamButton.addEventListener('click', ()=>{
+
+    hamButton.classList.toggle("change");
+    openNav();
+});
+
+close.addEventListener('click', ()=>{
+    closeNav();
+});
+
+function openNav() {
+    MobileNav.style.height = "100%";
+}
+
+function closeNav() {
+    MobileNav.style.height = "0%";
+}
